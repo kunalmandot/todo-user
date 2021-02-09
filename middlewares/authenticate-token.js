ee5f6ec36b3ecd15380ee009c39f6b089a372ddc
Kunal <kunalmandot1999@gmail.com>
2021-02-09 14:58:04 +0530

    integrated ESLint with airbnb code style

--- middlewares/authenticate-token.js
+++ middlewares/authenticate-token.js
@@ -3,6 +3,7 @@ const BlackListToken = require('../api/models/blackListToken');
 
 const throwServerError = (res) => res.status(500).json({ msg: 'Something went wrong.' });
 
+// eslint-disable-next-line consistent-return
 const authenticateToken = async (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -15,6 +16,7 @@ const authenticateToken = async (req, res, next) => {
         if (blackListedToken) {
             return res.status(400).json({ msg: 'Your token is already destroyed.' });
         }
+        // eslint-disable-next-line consistent-return
         jwt.verify(token, 'first application', (err, user) => {
             if (err) {
                 return res.status(403).json({ msg: 'Token mismatch occurred.' });
